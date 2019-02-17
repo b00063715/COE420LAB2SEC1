f8df1c4a79d0d7d6ae77f71b74c5ac66943a9970
b00063715 <b00063715@aus.edu>
2019-02-17 12:13:35 +0400

    Initial Commit

---
+++ src/coe420lab2/COE420LAB2.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420lab2;
+
+/**
+ *
+ * @author b00063715
+ */
+public class COE420LAB2 {
+public static void addFeatureA(){
+    
+    System.out.println("Feature A Added ");
+    
+}
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        addFeatureA();
+    }
+    
+}
